4e3421728d23293b11d754651562bf332df748e6
SASTACODERS <princekumar34379@gmail.com>
2024-10-23 00:48:39 +0530

    ᴀʟᴘʜᴀ

--- SONALI/plugins/tools/couples.py
+++ SONALI/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from SONALI.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="|| ˹𖤍 𝗔𝗟𝗣𝗛𝗔 𖤍˼ ||",
-            url=f"https://t.me/PURVI_SUPPORT",
+            text="ꪜ 𝛊 ɭ ɭ ᧘ 𝛊 𝛈",
+            url=f"https://t.me/oldskoolgc",
         ),
     ],
 ]
