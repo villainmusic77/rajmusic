4be2d6818760df15f3b176a07fd2d0615a70a353
VILLAIN <iamrkay420@gmail.com>
2024-11-06 15:49:29 +0530

    Update telegraph.py

--- SONALI/plugins/tools/telegraph.py
+++ SONALI/plugins/tools/telegraph.py
@@ -58,7 +58,7 @@ async def get_link_group(client, message):
                         [
                             [
                                 InlineKeyboardButton(
-                                    "𝖢ʀᴇᴀᴛᴇ ʙʏ 𝖳ᴇᴀᴍ 𝖯ᴜʀᴠɪ 𝖳ᴀᴘ ᴛᴏ 𝖲ᴇᴇ",
+                                    "𝖳ᴀᴘ ᴛᴏ 𝖲ᴇᴇ",
                                     url=upload_path,
                                 )
                             ]
